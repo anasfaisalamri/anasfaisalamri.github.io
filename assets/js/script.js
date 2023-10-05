1323a6ec1a69c8f9683fec1740e2d9300406de6c
anasfaisalamri <anasfaisalamri@gmail.com>
2023-10-05 21:20:21 +0700

    merubah bagian about dan portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,13 @@ toTop.forEach((e) => {
 });
 
 const year = document.querySelector(".year");
-let date = new Date();
-year.innerHTML = date.getFullYear();
+year.textContent = new Date().getFullYear();
 
 let tools = [
   "html5",
   "css3",
   "bootstrap",
   "javascript",
-  "reactjs",
   "github",
   "gitbash",
   "vscode",
@@ -39,19 +37,16 @@ bagianTools.innerHTML = isiTools;
 let project = document.querySelector(".project");
 let myProject = [
   {
-    title: "Calculator",
-    github: "https://anasfaisalamri.github.com/calculator",
-    url: "https://anasfaisalamri.github.io/calculator",
-  },
-  {
-    title: "To-do-List",
-    github: "https://github.com/anasfaisalamri/to-do-list",
-    url: "https://anasfaisalamri.github.io/to-do-list",
+    title: "Kontemplato",
+    // github: "https://kontemplato.refuzionc0de.my.id/",
+    url: "https://kontemplato.refuzionc0de.my.id/",
+    img: "kontemplato",
   },
   {
     title: "Wedding Invitation",
     github: "https://github.com/anasfaisalamri/wedding-inv",
     url: "https://anasfaisalamri.github.io/wedding-inv",
+    img: "wedding",
   },
 ];
 
@@ -60,12 +55,18 @@ let isiProject = `${myProject
     (e) =>
       `<div class="col-md-6 col-lg-4 mb-3" data-aos="fade-up" data-aos-offset="150" data-aos-delay="${(delay += 50)}">
         <div class="card">
-          <img class="card-img-top">
+          <img class="card-img-top" ${
+            e.img ? "src='assets/img/" + e.img + ".jpg'" : ""
+          }>
           <div class="card-body">
             <h5 class="card-title">${e.title}</h5>
-            <a href="${
+            ${
               e.github
-            }" target="_blank" class="btn btn-primary">Github</a>
+                ? "<a href=" +
+                  e.github +
+                  "target='_blank' class='btn btn-primary'>Github</a>"
+                : ""
+            }
             <a href="${e.url}" target="_blank" class="btn btn-primary">Open</a>
           </div>
         </div>
@@ -75,26 +76,138 @@ let isiProject = `${myProject
 
 project.innerHTML = isiProject;
 
-let designs = [
+let logoDesigns = [
+  "gorillaz",
   "cyborgguppy",
   "luwakmotobike",
   "kingfarmer",
   "homesteak",
   "seafoodjulid",
+  "buttalewangang",
+  "kazmy",
   "butetjayatas",
 ];
 
-let items = [];
+let logoSrc = [];
 
-designs.forEach((value, index) => {
-  items.push(`assets/img/thumb/${value}.png`);
+logoDesigns.forEach((value, index) => {
+  logoSrc.push(`assets/img/thumb/${value}.jpg`);
 });
 
-items.forEach((item) => {
+logoSrc.forEach((item) => {
+  let carouselItem = document.createElement("div");
+  carouselItem.setAttribute("class", "carousel-item");
+
+  let colEl = document.createElement("div");
+  colEl.setAttribute("class", "col-md-3");
+
+  let card = document.createElement("div");
+  card.setAttribute("class", "card border-0 me-2");
+
+  let cardImg = document.createElement("div");
+  cardImg.setAttribute("class", "card-img");
+
+  let anchor = document.createElement("a");
+  anchor.setAttribute("href", item);
+  anchor.setAttribute("data-toggle", "lightbox");
+  anchor.setAttribute("data-gallery", "logo-design");
+
   let img = document.createElement("img");
   img.setAttribute("src", item);
-  let design = document.querySelector(".design");
-  design.appendChild(img);
+  img.setAttribute("class", "card-img");
+
+  anchor.appendChild(img);
+  cardImg.appendChild(anchor);
+  card.appendChild(cardImg);
+  colEl.appendChild(card);
+  carouselItem.appendChild(colEl);
+
+  let logoDesignEl = document.querySelector(".logo-design");
+  logoDesignEl.appendChild(carouselItem);
+});
+
+let items = document.querySelectorAll(".carousel .carousel-item");
+items[0].setAttribute("class", "carousel-item active");
+
+items.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (let i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+
+let templateLogoDesigns = [
+  "ayamgeprek",
+  "ayam",
+  "bakso",
+  "burger",
+  "jus",
+  "boba",
+  "skincare",
+];
+
+let templateLogoSrc = [];
+
+templateLogoDesigns.forEach((value, index) => {
+  templateLogoSrc.push(`assets/img/template/${value}.jpg`);
+});
+
+templateLogoSrc.forEach((item) => {
+  let carouselItem = document.createElement("div");
+  carouselItem.setAttribute("class", "carousel-item template");
+
+  let colEl = document.createElement("div");
+  colEl.setAttribute("class", "col-md-3");
+
+  let card = document.createElement("div");
+  card.setAttribute("class", "card border-0 me-2");
+
+  let cardImg = document.createElement("div");
+  cardImg.setAttribute("class", "card-img");
+
+  let anchor = document.createElement("a");
+  anchor.setAttribute("href", item);
+  anchor.setAttribute("data-toggle", "lightbox");
+  anchor.setAttribute("data-gallery", "template-design");
+
+  let img = document.createElement("img");
+  img.setAttribute("src", item);
+  img.setAttribute("class", "card-img");
+
+  anchor.appendChild(img);
+  cardImg.appendChild(anchor);
+  card.appendChild(cardImg);
+  colEl.appendChild(card);
+  carouselItem.appendChild(colEl);
+
+  let logoDesignEl = document.querySelector(".template-design");
+  logoDesignEl.appendChild(carouselItem);
+});
+
+let itemsTemplate = document.querySelectorAll(
+  ".carousel .template-design .template"
+);
+itemsTemplate[0].setAttribute("class", "carousel-item template active");
+
+itemsTemplate.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (let i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = itemsTemplate[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
 });
 
 let contacts = [
